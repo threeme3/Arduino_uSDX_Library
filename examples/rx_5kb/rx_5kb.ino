72d735f0f125c202a79f18e7751c3097435bd095
threeme3 <threeme3@hotmail.com>
2024-03-19 21:56:26 +0100

    Update rx_5kb.ino

--- examples/rx_5kb/rx_5kb.ino
+++ examples/rx_5kb/rx_5kb.ino
@@ -1,4 +1,4 @@
-// rx_5kb.ino - https://github.com/threeme3/usdx-sketch
+// rx_5kb.ino - https://github.com/threeme3/Arduino_uSDX_Library
 //
 //  Copyright 2019, 2020, 2021, 2022, 2023, 2024   Guido PE1NNZ <pe1nnz@amsat.org>
 //
